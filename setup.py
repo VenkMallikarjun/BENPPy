ffa23b92837694c089750430ff45d0ff34a6aaf4
Venk <36739430+VenkMallikarjun@users.noreply.github.com>
2020-02-21 10:53:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 configuration = {
     'name' : 'BENPPy',
-    'version': '2.3.3',
+    'version': '2.4.1',
     'description' : 'BayesENproteomics in Python',
     'long_description' : readme(),
     'classifiers' : [
